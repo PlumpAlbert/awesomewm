5611e6fc0d7bfdd51d0f29ff76377a72659d8563
plump_albert <plumpalbert@gmail.com>
2023-03-20 21:57:46 +0300

    updated music notification text

--- ui/dings/actions/music.lua
+++ ui/dings/actions/music.lua
@@ -2,9 +2,8 @@ local naughty = require("naughty")
 local bling = require("modules.bling")
 local playerctl = bling.signal.playerctl.lib()
 
-playerctl:connect_signal("metadata",
-  function(_, title, artist, album_path, album, new, player_name)
-    if new == true then
-      naughty.notify({ title = 'New Song', text = title .. " by " .. artist, image = album_path })
-    end
-  end)
+playerctl:connect_signal("metadata", function(_, title, artist, album_path, album, new, player_name)
+	if new == true then
+		naughty.notify({ title = title, text = "by " .. artist, image = album_path })
+	end
+end)
