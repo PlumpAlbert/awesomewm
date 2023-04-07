b3b9a615ce14d00fdeeb6922bc28d7545839c043
plump_albert <plumpalbert@gmail.com>
2023-04-07 07:15:46 +0300

    updated everforest theme

---
+++ theme/palettes/everforest_light.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.name = "everforest_light"
+M.wall = "forest_stairs.jpg"
+M.pri = "#90bd9b"
+M.warn = "#dbb678"
+M.gtk = "everforestgtk"
+M.err = "#ce6568"
+M.ok = "#7fbbb3"
+M.dis = "#d699b6"
+M.bg = "#fdf6e3"
+M.mbg = "#f4f0d9"
+M.bg2 = "#e6e2cc"
+M.bg3 = "#e0dcc7"
+M.bg4 = "#bdc3af"
+M.fg = "#5c6a72"
+M.fg2 = "#708089"
+M.fg3 = "#a6b0a0"
+M.fg4 = "#829181"
+
+return M
