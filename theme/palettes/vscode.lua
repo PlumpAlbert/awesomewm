2b8938724ddc31adcc941445e99fb11e4dcb5b9a
plump_albert <plumpalbert@gmail.com>
2023-04-22 12:55:51 +0300

    created vscode palette

---
+++ theme/palettes/vscode.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.name  = 'vscode'
+M.wall  = '~/.wallpaper'
+M.ok    = "#608b4e"
+M.warn  = "#dcdcaa"
+M.gtk   = "Pop"
+M.err   = "#f44747"
+M.pri   = "#569cd6"
+M.dis   = "#c678dd"
+M.bg    = "#0c0c0c"
+M.bg2   = "#191919"
+M.bg3   = "#1b1b1b"
+M.mbg   = "#181817"
+M.bg4   = "#272727"
+M.fg    = "#dfdddd"
+M.fg2   = "#b7b7b7"
+M.fg3   = "#5a5858"
+M.fg4   = "#838383"
+
+
+return M
