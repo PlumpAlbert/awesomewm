187928985ddde284252d770c392e2fe581697f7f
plump_albert <plumpalbert@gmail.com>
2023-04-26 22:00:50 +0300

    added nightfly theme

---
+++ theme/palettes/nightfly.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.name  = 'nightfly'
+M.wall  = '~/.wallpaper'
+M.ok    = "#a1cd5e"
+M.warn  = "#e3d18a"
+M.gtk   = "Pop"
+M.err   = "#fc514e"
+M.pri   = "#82aaff"
+M.dis   = "#c792ea"
+M.bg    = "#080808"
+M.bg2   = "#181818"
+M.bg3   = "#1b1b1b"
+M.mbg   = "#181818"
+M.bg4   = "#272727"
+M.fg    = "#acb4c2"
+M.fg2   = "#9ca1aa"
+M.fg3   = "#5a585a"
+M.fg4   = "#838383"
+
+
+return M
