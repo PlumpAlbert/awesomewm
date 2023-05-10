700b5eb48cc93e1a3250377dcb9d93117e582c2f
plump_albert <plumpalbert@gmail.com>
2023-05-10 18:28:27 +0300

    added catppuccin themes

---
+++ theme/palettes/catppuccin-latte.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.name = "catppuccin-latte"
+M.wall = "waves.jpg"
+M.ok = "#40a02b"
+M.warn = "#fe640b"
+M.gtk = "Pop"
+M.err = "#d20f39"
+M.pri = "#ea76cb"
+M.dis = "#7287fd"
+M.bg = "#eff1f5"
+M.mbg = "#e6e9ef"
+M.bg2 = "#dce0e8"
+M.bg3 = "#ccd0da"
+M.bg4 = "#bcc0cc"
+M.fg = "#4c4f69"
+M.fg2 = "#5c5f77"
+M.fg3 = "#6c6f85"
+M.fg4 = "#7c7f93"
+
+return M
---
+++ theme/palettes/catppuccin.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.name = "catppuccin"
+M.wall = "waves.jpg"
+M.ok = "#a6da95"
+M.warn = "#f5a97f"
+M.gtk = "Pop-dark"
+M.err = "#ed8796"
+M.pri = "#8aadf4"
+M.dis = "#c6a0f6"
+M.bg = "#181926"
+M.mbg = "#1e2030"
+M.bg2 = "#24273a"
+M.bg3 = "#363a4f"
+M.bg4 = "#494d64"
+M.fg = "#cad3f5"
+M.fg2 = "#b8c0e0"
+M.fg3 = "#a5adcb"
+M.fg4 = "#939ab7"
+
+return M
