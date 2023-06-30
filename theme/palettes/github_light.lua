e0e9c16ce4e858fabcde679ab540ee56d0ad6387
Матвей Федин <plumpalbert@gmail.com>
2023-06-30 12:19:59 +0300

    added github_light theme

---
+++ theme/palettes/github_light.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.name = "github_light"
+M.wall = "waves.jpg"
+M.ok = "#28a745"
+M.warn = "#dbab09"
+M.gtk = "Pop"
+M.err = "#d73a49"
+M.pri = "#5a32a3"
+M.dis = "#0366d6"
+M.bg = "#ffffff"
+M.mbg = "#e6e9ef"
+M.bg2 = "#dce0e8"
+M.bg3 = "#ccd0da"
+M.bg4 = "#bcc0cc"
+M.fg = "#24292e"
+M.fg2 = "#31383F"
+M.fg3 = "#4C5761"
+M.fg4 = "#959da5"
+
+return M
